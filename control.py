be83248cd8243436b8537bbab0daf92e3ed0d246
Yunseong Lee <yunseong@apache.org>
2019-04-09 11:22:10 +0900

    Update the values of termination flags

--- control.py
+++ control.py
@@ -1,6 +1,6 @@
 class TerminationFlag:
   """An enum class for representing various termination states."""
-  UNSET = 0
+  UNSET = -1
+  TARGET_NUM_VIDEOS_REACHED = 0
   FILENAME_QUEUE_FULL = 1
   FRAME_QUEUE_FULL = 2
-  TARGET_NUM_VIDEOS_REACHED = 3
