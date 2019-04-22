65385014541e0b4b8027e75785b8632d2399900c
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2019-04-22 19:32:32 +0900

    Decouple NN implementation from runner logic (#21)
    * Decouple r2p1d impl from runner
    
    * Rename the class R2P1D to R2P1DRunner in model.py

---
+++ models/r2p1d/model.py
@@ -0,0 +1,25 @@
+import torch
+
+from models.r2p1d.network import R2Plus1DClassifier, SpatioTemporalResBlock
+from runner_model import RunnerModel
+
+CKPT_PATH = '/cmsdata/ssd0/cmslab/Kinetics-400/ckpt/model_data.pth.tar'
+
+class R2P1DRunner(RunnerModel):
+  """RunnerModel impl of the R(2+1)D model for RnB benchmark runners."""
+  def __init__(self, device, num_classes=400, layer_sizes=[2,2,2,2],
+                     block_type=SpatioTemporalResBlock):
+    super(R2P1DRunner, self).__init__(device)
+
+    self.model = R2Plus1DClassifier(num_classes, layer_sizes, block_type) \
+                     .to(device)
+    ckpt = torch.load(CKPT_PATH, map_location=device)
+    self.model.load_state_dict(ckpt['state_dict'])
+
+
+  def input_shape(self):
+    return (10, 3, 8, 112, 112)  
+
+
+  def __call__(self, input):
+    return self.model(input)
