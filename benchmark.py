b9d55e1b5256d3d1f50352257033c6793afd1863
Yunseong Lee <yunseong@apache.org>
2019-04-22 20:40:42 +0900

    Remove the previous code and add comments

--- benchmark.py
+++ benchmark.py
@@ -18,12 +18,9 @@ def sanity_check(args):
   The function 'sanity_check' checks the arguments and terminates when an invalid state is observed. 
 
   The program will terminate in the following situations:
-  TODO Remove
-  1) The current benchmark expects arguments to be positive integers, thus any argument 
-     less than 1 will be considered invalid and lead the program to terminate.  
-  2) The values given for environment variable 'CUDA_VISIBLE_DEVICES' will be checked
+  1) The values given for environment variable 'CUDA_VISIBLE_DEVICES' will be checked
      to see if valid argument is given, and the program will terminate if not so. 
-  3) Here, we will regard GPUs with no process running along with no consumption in memory as 'free'. 
+  2) Here, we will regard GPUs with no process running along with no consumption in memory as 'free'. 
      If a GPU has no process running, but is consuming some memory, we will regard the GPU as 'not-free', 
      and prevent users from using it. If user requires more GPUs than the number of GPUs that are 
      both accessible and free, the program will also terminate. 
@@ -32,25 +29,10 @@ def sanity_check(args):
   import sys
   from py3nvml import py3nvml
 
-  # Case 1: Check whether arguments are positive integers 
-  #invalid_argument = []
-  #for arg in vars(args).keys():
-  #  if isinstance(vars(args)[arg], str):
-  #    # the check below is invalid for strings
-  #    continue
-
-  #  if vars(args)[arg] < 1:  
-  #    invalid_argument.append(arg) 
-  
-  #if len(invalid_argument) > 0:
-  #  for x in invalid_argument:
-  #    print('[WARNING] Invalid number for %s. (%d) ' % (x, vars(args)[x]))
-  #  sys.exit()
-
-  # Case 2: Return 'ValueError' if any types other than integers are set for this environment variable
+  # Case 1: Return 'ValueError' if any types other than integers are set for this environment variable
   visible_gpu_idx = sorted([int(x) for x in os.environ['CUDA_VISIBLE_DEVICES'].split(',')]) 
    
-  # Case 3: Check whether user requires more GPUs than the number of GPUs that are both accessible and free
+  # Case 2: Check whether user requires more GPUs than the number of GPUs that are both accessible and free
   py3nvml.nvmlInit()
  
   # Find the indices of GPUs that are both free and visible
