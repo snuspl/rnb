00298c79102f5d233292436a4a417c3645763f02
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2019-06-13 17:43:29 +0900

    Deprecate unused R(2+1)D net implementations (#47)
    * deprecate unused r2p1d net implementations
    
    * Rename R2P1DLayerRunner -> R2P1DRunner

--- models/r2p1d/model.py
+++ models/r2p1d/model.py
@@ -5,40 +5,11 @@ import sys
 from models.r2p1d.sampler import R2P1DSampler
 from models.r2p1d.network import R2Plus1DClassifier, SpatioTemporalResBlock
 from models.r2p1d.network import R2Plus1DLayerWrapper
-from models.r2p1d.network import R2Plus1DLayer12Wrapper
-from models.r2p1d.network import R2Plus1DLayer345Wrapper
 from runner_model import RunnerModel
 
 CKPT_PATH = '/cmsdata/ssd0/cmslab/Kinetics-400/ckpt/model_data.pth.tar'
 
 class R2P1DRunner(RunnerModel):
-  """RunnerModel impl of the R(2+1)D model for RnB benchmark runners."""
-  def __init__(self, device, num_classes=400, layer_sizes=[2,2,2,2],
-                     block_type=SpatioTemporalResBlock):
-    super(R2P1DRunner, self).__init__(device)
-
-    self.model = R2Plus1DClassifier(num_classes, layer_sizes, block_type) \
-                     .to(device)
-    ckpt = torch.load(CKPT_PATH, map_location=device)
-    self.model.load_state_dict(ckpt['state_dict'])
-
-    # warm up GPU with a few inferences
-    inp_shape = self.input_shape()
-    stream = torch.cuda.current_stream()
-    tmp = torch.randn(*inp_shape, dtype=torch.float32).cuda()
-    for _ in range(3):
-      _ = self.model(tmp)
-      stream.synchronize()
-
-
-  def input_shape(self):
-    return (10, 3, 8, 112, 112)
-
-
-  def __call__(self, input):
-    return self.model(input)
-
-class R2P1DLayerRunner(RunnerModel):
   """RunnerModel that can create any connected subset of the R(2+1)D model for RnB benchmark runners.
   
   start_index and end_index are assumed to be 1-indexed. 
@@ -54,7 +25,7 @@ class R2P1DLayerRunner(RunnerModel):
                  5: (10, 256, 2, 14, 14) }
 
   def __init__(self, device, start_index=1, end_index=5, num_classes=400, layer_sizes=None, block_type=SpatioTemporalResBlock):
-    super(R2P1DLayerRunner, self).__init__(device)
+    super(R2P1DRunner, self).__init__(device)
     
     if start_index < 1:
       print('[ERROR] Wrong layer index for the starting layer! The start_index (%d) should be more than or equal to 1.' % start_index) 
@@ -93,71 +64,6 @@ class R2P1DLayerRunner(RunnerModel):
     
   def input_shape(self):
     return self.input_dict[self.start_index]
-
-  def __call__(self, input):
-    return self.model(input)
-
-class R2P1DLayer12Runner(RunnerModel):
-  """RunnerModel impl for the first two layers of the R(2+1)D model."""
-  def __init__(self, device, layer_size=2, block_type=SpatioTemporalResBlock):
-    super(R2P1DLayer12Runner, self).__init__(device)
-
-    self.model = R2Plus1DLayer12Wrapper(layer_size, block_type).to(device)
-    ckpt = torch.load(CKPT_PATH, map_location=device)
-
-    # filter out weights that are not used in this model
-    state_dict = {k:v for k,v in ckpt['state_dict'].items() if
-                  k.startswith('res2plus1d.conv1') or
-                  k.startswith('res2plus1d.conv2')}
-    self.model.load_state_dict(state_dict)
-
-    # warm up GPU with a few inferences
-    inp_shape = self.input_shape()
-    stream = torch.cuda.current_stream()
-    tmp = torch.randn(*inp_shape, dtype=torch.float32).cuda()
-    for _ in range(3):
-      _ = self.model(tmp)
-      stream.synchronize()
-
-
-  def input_shape(self):
-    return (10, 3, 8, 112, 112)
-
-
-  def __call__(self, input):
-    return self.model(input)
-
-
-class R2P1DLayer345Runner(RunnerModel):
-  """RunnerModel impl for the last three layers of the R(2+1)D model."""
-  def __init__(self, device, num_classes=400, layer_sizes=[2,2,2],
-                     block_type=SpatioTemporalResBlock):
-    super(R2P1DLayer345Runner, self).__init__(device)
-
-    self.model = R2Plus1DLayer345Wrapper(num_classes, layer_sizes, block_type) \
-                     .to(device)
-    ckpt = torch.load(CKPT_PATH, map_location=device)
-
-    # filter out weights that are not used in this model
-    state_dict = {k:v for k,v in ckpt['state_dict'].items() if
-                      k.startswith('res2plus1d.conv3') or
-                      k.startswith('res2plus1d.conv4') or
-                      k.startswith('res2plus1d.conv5') or
-                      k.startswith('linear')}
-    self.model.load_state_dict(state_dict)
-
-    # warm up GPU with a few inferences
-    inp_shape = self.input_shape()
-    stream = torch.cuda.current_stream()
-    tmp = torch.randn(*inp_shape, dtype=torch.float32).cuda()
-    for _ in range(3):
-      _ = self.model(tmp)
-      stream.synchronize()
-
-
-  def input_shape(self):
-    return (10, 64, 8, 56, 56)
-
 
   def __call__(self, input):
     return self.model(input)
