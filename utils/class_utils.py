33ac75b4b1858ddf8e2839d4442c73065290d77f
Yunseong Lee <yunseong@apache.org>
2019-07-05 06:41:54 +0000

    Add a utility function load_class

--- utils/class_utils.py
+++ utils/class_utils.py
@@ -1,7 +1,11 @@
 import sys
-def get_class(module_path, kwargs):
-    delimiter_idx = module_path.rfind('.')
-    module_path = module_path[:delimiter_idx]
-    class_name = module_path[delimiter_idx+1:]
+
+def load_class(full_class_path):
+    """Loads a class object from a full path to the class. 
+    """
+    delimiter_idx = full_class_path.rfind('.')
+    module_path = full_class_path[:delimiter_idx]
+    class_name = full_class_path[delimiter_idx+1:]
+
     module = __import__(module_path, fromlist=(class_name))
     return getattr(module, class_name)
