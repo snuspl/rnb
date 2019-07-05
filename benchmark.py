4e22bf462ce77b528bb9e02497f607f765c6c2ad
Yunseong Lee <yunseong@apache.org>
2019-07-05 18:44:09 +0900

    Add missing import

--- benchmark.py
+++ benchmark.py
@@ -126,6 +126,7 @@ if __name__ == '__main__':
   import shutil
   import time
   from arg_utils import *
+  from utils.class_utils import load_class
   from datetime import datetime as dt
   from torch.multiprocessing import Queue, Process, Value, Barrier
 
