0e46d20df2f592d35ebc751a04ee93734b23b2be
Yunseong Lee <yunseong@apache.org>
2019-07-07 18:11:10 +0900

    Decouple dataset-specific part from client implementation (#55)
    * Add video_path_iterator as an entry of configuration
    * Instantiate VideoPathIterator in client

---
+++ video_path_provider.py
@@ -0,0 +1,14 @@
+class VideoPathIterator:
+  """Interface class for iterating video file paths to load."""
+  def __init__(self):
+    """Empty initialization method."""
+    pass
+
+  def __iter__(self):
+    """Returns the iterator object.
+
+    We highly recommend using itertools.cycle to let the benchmark process
+    the desired number of videos regardless of the number of existing
+    videos in the file system.
+    """
+    raise NotImplementedError
